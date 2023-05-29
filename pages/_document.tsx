95ecc1149647428f9945d4efb14a6167332c58b9
shdomi8599 <shdomi@naver.com>
2023-05-30 08:33:59 +0900

    feat : 초기 세팅 및 파이어베이스 DB 연결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,61 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
 
-export default function Document() {
+type Props = {
+  styles: React.ReactElement[];
+};
+
+const MyDocument = ({ styles }: Props) => {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        {/* <meta
+          name="description"
+          content="개발자와 디자이너 등 다양한 색을 가진 각 분야의 전문가들이 모여 더 멋진 앞날을 꿈꾸는 곳입니다."
+        />
+        <meta property="og:title" content="사이드 퀘스트" />
+        <meta
+          property="og:description"
+          content="개발자와 디자이너 등 다양한 색을 가진 각 분야의 전문가들이 모여 더 멋진 앞날을 꿈꾸는 곳입니다."
+        />
+        <meta property="og:image" content="/images/logo.svg" />
+        <meta property="og:url" content="https://sidequest.co.kr/" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="사이드 퀘스트" />
+        <meta
+          name="twitter:description"
+          content="개발자와 디자이너 등 다양한 색을 가진 각 분야의 전문가들이 모여 더 멋진 앞날을 꿈꾸는 곳입니다."
+        />
+        <meta name="twitter:image" content="/images/logo.svg" />
+        <link rel="canonical" href="https://sidequest.co.kr/" />
+        <link rel="icon" href="/favicon.png" /> */}
+        {styles}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+MyDocument.getInitialProps = async (
+  ctx: DocumentContext
+): Promise<DocumentInitialProps & { styles: React.ReactElement[] }> => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = sheet.getStyleElement();
+    return { ...initialProps, styles };
+  } finally {
+    sheet.seal();
+  }
+};
+
+export default MyDocument;
