1c987312ee252c36ab615597c52d0fed1891de7d
shdomi8599 <shdomi@naver.com>
2023-05-30 13:22:41 +0900

    feat : firebase db 테스트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/App.css'
-import type { AppProps } from 'next/app'
+import "@/styles/App.css";
+import type { AppProps } from "next/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCU9iaAP6DzVRP334iEKNjQY7HtzhU1jro",
+  authDomain: "modoosystem-af119.firebaseapp.com",
+  databaseURL:
+    "https://modoosystem-af119-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "modoosystem-af119",
+  storageBucket: "modoosystem-af119.appspot.com",
+  messagingSenderId: "783188813845",
+  appId: "1:783188813845:web:bd0b70965a5e505bbd861c",
+  measurementId: "G-3718WK18B2",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import Head from "next/head";
+import { collection, addDoc, doc, setDoc } from "firebase/firestore";
+import { db } from "./_app";
+
 export default function Home() {
+  const test = async () => {
+    await setDoc(doc(db, "cities", "LA"), {
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA",
+    }).then((res) => console.log(res));
+  };
   return (
     <>
       <Head>
         <title>모두시스템</title>
       </Head>
-      <div>dsadsa</div>
+      <button onClick={test}>dsadsa</button>
     </>
   );
 }
