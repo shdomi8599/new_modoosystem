57a8cc089e25f8a1e1d3d6e435a4aa0f2b80a389
shdomi8599 <shdomi@naver.com>
2023-06-01 08:31:01 +0900

    feat : HEADER_NAV 상수

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,14 @@
 import Head from "next/head";
-import { Button, DatePicker, Space } from "antd";
-import { useState } from "react";
-import { Dayjs } from "dayjs";
 
 export default function Home() {
-  const [date, setDate] = useState<Dayjs | null>(null);
-  const handleChange = (value: Dayjs | null) => {
-    setDate(value);
-    console.log(date?.format("YYYY-MM-DD"));
-  };
   return (
     <>
       <Head>
         <title>모두시스템</title>
       </Head>
       <main>
-        <nav></nav>
-        <div>
-          <Space>
-            <DatePicker onChange={handleChange} />
-            <Button type="primary">Primary Button</Button>
-          </Space>
-        </div>
+        <nav>네비바</nav>
+        <div></div>
       </main>
     </>
   );
