69232a57c47ccdded8052e79ec85e5446c50371f
shdomi8599 <shdomi@naver.com>
2023-06-01 06:48:02 +0900

    refactor : 파이어베이스 CRUD 함수 util화

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,12 @@
 import Head from "next/head";
-import { collection, addDoc, doc, setDoc, getDocs } from "firebase/firestore";
-import { db } from "./_app";
 
 export default function Home() {
-  /**
-   * 컬렉션 세팅
-   * 셋팅과 동시에 데이터를 추가할 수 있음
-   */
-  const setData = () => {
-    setDoc(doc(db, "citiesddd", "LA"), {
-      name: "Los Angeles",
-      state: "CAdwdwqd",
-      country: "USA",
-    }).then((res) => console.log(res));
-  };
-
-  /**
-   * 셋팅되있는 컬렉션에 데이터를 추가
-   */
-  const addData = async () => {
-    const newDocRef = await addDoc(collection(db, "citiesddd"), {
-      name: "New City",
-      state: "New State",
-      country: "New Country",
-    });
-    console.log("Added document with ID: ", newDocRef.id);
-  };
-
-  /**
-   * 컬렉션 데이터 단일 조회
-   */
-  const getData = async () => {
-    const querySnapshot = await getDocs(collection(db, "citiesddd"));
-    querySnapshot.forEach((doc) => {
-      console.log(doc.data());
-    });
-  };
-
-  /**
-   * 한 컬렉션에 대한 전체 데이터 조회
-   */
-  const getAllData = async () => {
-    const querySnapshot = await getDocs(collection(db, "citiesddd"));
-    const dataArray = querySnapshot.docs.map((doc) => doc.data());
-    console.log(dataArray);
-  };
-
   return (
     <>
       <Head>
         <title>모두시스템</title>
       </Head>
-      <button onClick={setData}>작성</button>
-      <button onClick={addData}>추가</button>
-      <button onClick={getData}>조회</button>
-      <button onClick={getAllData}>전체조회</button>
+      <div>ss</div>
     </>
   );
 }
---
+++ util/server/index.ts
@@ -0,0 +1,61 @@
+import { db } from "@/pages/_app";
+import {
+  collection,
+  addDoc,
+  doc,
+  setDoc,
+  getDocs,
+  deleteDoc,
+} from "firebase/firestore";
+
+/**
+ * 컬렉션명과 이름이 일치하는 데이터의 값을 수정할 수 있음
+ */
+export const updateData = async <T extends {}>(
+  collectionName: string,
+  id: string,
+  data: T
+) => {
+  const docRef = doc(db, collectionName, id);
+  await setDoc(docRef, data);
+};
+
+/**
+ * 컬렉션명과 ID가 일치하는 데이터를 삭제하는 함수
+ */
+export const deleteData = async (collectionName: string, id: string) => {
+  const docRef = doc(db, collectionName, id);
+  await deleteDoc(docRef);
+  console.log("Document deleted successfully");
+};
+
+/**
+ * 컬렉션에 데이터를 추가
+ * 셋팅과 동시에 데이터를 추가할 수 있음
+ */
+export const addData = async <T extends {}>(
+  collectionName: string,
+  data: T
+) => {
+  const newDocRef = await addDoc(collection(db, collectionName), data);
+  console.log("Added document with ID: ", newDocRef.id);
+};
+
+/**
+ * 컬렉션 데이터 단일 조회
+ */
+export const getData = async (collectionName: string) => {
+  const querySnapshot = await getDocs(collection(db, collectionName));
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, doc.data());
+  });
+};
+
+/**
+ * 한 컬렉션에 대한 전체 데이터 조회
+ */
+export const getAllData = async (collectionName: string) => {
+  const querySnapshot = await getDocs(collection(db, collectionName));
+  const dataArray = querySnapshot.docs.map((doc) => doc.data());
+  console.log(dataArray);
+};
