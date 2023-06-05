e88d735e7feb4f199ee0cbed6a3ab4c4d2d1b090
shdomi8599 <shdomi@naver.com>
2023-06-06 03:05:43 +0900

    feat : 더미데이터 콘솔

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import HeadTitle from "@/components/common/HeadTitle";
 import styled from "styled-components";
+import installStatuses from "@/dummy/installStatuses.json";
 
 export default function HomePage() {
+  console.log(installStatuses);
   return (
     <>
       <HeadTitle name="모두시스템" />
