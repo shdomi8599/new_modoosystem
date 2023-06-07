1a6b7a0f98b50d547a55acf919a9b790c66d51d6
shdomi8599 <shdomi@naver.com>
2023-06-07 18:31:58 +0900

    feat : swc 컴파일 기능

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    forceSwcTransforms: true, //실험적 기능 체험해보는 중,
+  },
 };
 
 module.exports = nextConfig;
