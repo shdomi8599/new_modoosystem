470713b6efe6efc39e563f68219f9c8dd0a1ca79
shdomi8599 <shdomi@naver.com>
2023-06-07 18:03:32 +0900

    test : deploy

--- util/api/index.ts
+++ util/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://new-modoosystem.vercel.app",
   timeout: 3000,
   headers: {
     "Content-Type": "application/json",
