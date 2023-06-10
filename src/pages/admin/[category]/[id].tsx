e7b3f7314a2fc6849f34220f7801c60acaf157c5
shdomi8599 <shdomi@naver.com>
2023-06-10 12:47:58 +0900

    feat : view페이지 주석 추가

--- src/pages/admin/[category]/[id].tsx
+++ src/pages/admin/[category]/[id].tsx
@@ -7,10 +7,12 @@ import { useRecoilState } from "recoil";
 import useCheckAdmin from "@/hooks/useCheckAdmin";
 
 /**
- * react-query dehydratedState를 사용하다보니 최초 데이터 없이
- * ViewPage 컴포넌트 안에서 사용되는 데이터의 구조분해에 실패하여,
- * 어쩔수없이 admin페이지에서도 SSR을 강제로 사용하게되는 상황이 와버렸다.
- * 다른 해결책을 찾기 전까진 일단 현상유지하고 나중에 리팩토링을 해야겠다.
+ * 최초 데이터 없이 ViewPage 컴포넌트 안에서 사용되는 데이터를 구조분해를
+ * 시도하는 바람에 에러가 발생하여, 어쩔수없이 admin페이지에서도 SSR을
+ * 강제로 사용하게되는 상황이 와버렸다. 다른 해결책을 찾기 전까진 일단
+ * 현상유지하고 나중에 리팩토링을 해봐야겠다.
+ * 생각난 해결방법
+ * 1. 데이터를 옵셔널체이닝으로 변경한다.
  */
 
 const AdminViewPage = () => {
