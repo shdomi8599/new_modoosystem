b30ae1511cd4efe5b3862f2b44412175da143c84
shdomi8599 <shdomi@naver.com>
2023-06-18 19:27:31 +0900

    fix : generatePaginationProps 배치 조정

--- src/pages/service/announcements/index.tsx
+++ src/pages/service/announcements/index.tsx
@@ -1,10 +1,6 @@
 import { Announcement } from "@/types/pageData";
-import { generatePaginationProps } from "@/util/ssr";
-
 import PaginationPage from "@/components/page/PaginationPage";
-import { QueryClient, dehydrate } from "react-query";
-import { REACT_QUERY_OPTIONS } from "@/datas/constants/constants";
-import { getPageData } from "@/util/api";
+import { generatePaginationProps } from "@/util/ssr";
 
 const endPoint = "announcements";
 const initialPage = 1;
@@ -14,15 +10,9 @@ const AnnouncementPage = () => PaginationPage<Announcement>({ endPoint });
 export default AnnouncementPage;
 
 export async function getServerSideProps() {
-  const queryClient = new QueryClient(REACT_QUERY_OPTIONS);
-
-  await queryClient.prefetchQuery([endPoint, initialPage], () =>
-    getPageData<Announcement>(endPoint, initialPage, initialSize)
+  return generatePaginationProps<Announcement>(
+    endPoint,
+    initialPage,
+    initialSize
   );
-
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
 }
